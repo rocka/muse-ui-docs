b75b4e96d93c883ce6153111c09f0b76b4f15959
Rocka <i@rocka.me>
2023-07-09 16:30:27 +0800

    build: use relative publicPath

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  env: {
+    development: {
+      compact: false
+    }
+  },
   presets: [
     '@vue/app'
   ]
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const VueMarkDownOptions = require('./scripts/vue-markdown-loader.conf');
 module.exports = {
   productionSourceMap: false,
   parallel: false,
+  publicPath: '.',
   chainWebpack (config) {
     config.resolve.extensions.add('.md');
     config.resolve.alias.set('muse-ui', '@rocka/muse-ui');
